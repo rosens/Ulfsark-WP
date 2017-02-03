e10e426103acc32d79f84f87f2da11b4302af3c2
Nacho Malo <wesopa@gmail.com>
2017-02-03 23:22:15 +0100

    Añadidas fuentes y fondo para el tema. Añadido fondo rojo para cada sección

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -2,3 +2,4 @@
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'ulfsark_components' ); ?></button>
 	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu' ) ); ?>
 </nav><!-- #site-navigation -->
+<img class ="header-logo-full" src="wp-content/themes/ulfsark_components/assets/img/logo-header-full.png">
--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="//db.onlinewebfonts.com/c/d0b9458c6d1bf76bb55624385cf59c84?family=Celtic+Garamond+the+2nd" rel="stylesheet" type="text/css"/>
 
 <?php wp_head(); ?>
 </head>
